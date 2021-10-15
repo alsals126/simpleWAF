3151ed54efb1e8b37c4fc42ddb644a8931d5d4a9
Jeongmin Oh <9970mia@naver.com>
2021-10-15 16:02:16 +0900

    back up

--- ui/view/logview.js
+++ ui/view/logview.js
@@ -1,54 +1,118 @@
-// 다시업로드 하기위해 주석 추가함
 import React, { useState} from 'react'
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 import "../css/logview.css"
 
+let endpoint = "http://127.0.0.1:8080";
+
+function dataAccess(ip, startDate, endDate) {
+    const param = new URLSearchParams();
+    console.log(ip)
+
+    if(ip!="") {
+        param.append('ip', ip)
+    }
+    if(startDate!="날짜선택") {
+        param.append('startDate', startDate)
+    }
+    if(endDate!="날짜선택") {
+        param.append('endDate', endDate)
+    }    
+
+    console.log(param)
+    var tem = []
+        axios.get(endpoint+"/log", param)
+        .then((res) => {
+            // response  
+            // if(res.data != null){
+            //     res.data.forEach((item) => {
+            //         var ipInfo = { id: item.Id, ip: item.Ip };
+            //         tem = tem.concat(ipInfo)
+            //     })  
+            //     this.setState({
+            //         loading: true,
+            //         Iplist: tem
+            //     })
+            // }
+        }).catch((error) => {
+            alert('에러발생\n관리자에게 문의해주세요')
+            console.log(error)
+            // this.setState({
+            //     loading: false
+            // })
+        })
+}
 const DatePickerOne = (props) => {
     const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date(null));
-    const [text, setText] = useState("기간 설정")
-    const onChange = (dates) => {
-        const [start, end] = dates
-        console.log(start)
-        setStartDate(start)
-        setEndDate(end)
-        setText(()=>{
-            const startDay = start.getFullYear().toString() + '.'
-                + (start.getMonth()+1).toString()
-            return startDate
-        })
-        if(start!=null && end!=null){ //마지막일 클릭시 datepicker 사라짐
-            props.showDatePicker()
+    const [endDate, setEndDate] = useState(new Date());
+  
+    const onChange1 = (start) => {
+        if (start <= endDate) {
+            setStartDate(start)
+            props.setStartText(
+                start.getFullYear().toString() + '.'
+                    + (start.getMonth()+1).toString() + '.'
+                    + start.getDate().toString()
+            )
+        }
+    }
+    const onChange2 = (end) => {
+        if (startDate <= end) {
+            setEndDate(end)
+            props.setEndText(
+                end.getFullYear().toString() + '.'
+                    + (end.getMonth()+1).toString() + '.'
+                    + end.getDate().toString()
+            )
         }
     }
 
     return(
-        <DatePicker
-            selected={startDate}
-            onChange={onChange}
-            startDate={startDate}
-            endDate={endDate}
-            selectsRange
-            value={text}
-        />
+        <>
+            <DatePicker
+                selectsStart    
+                selected={startDate}
+                onChange={onChange1}
+                startDate={startDate}
+                endDate={endDate}
+                value={props.startText}
+            />
+            <DatePicker
+                selectsEnd
+                selected={endDate}
+                onChange={onChange2}
+                startDate={startDate}
+                endDate={endDate}
+                value={props.endText}
+            />
+        </>
     );
 }
-function LogView(props) {
-    const [openDatePicker, setOpenDatePicker] = useState(false);
-    const showDatePicker = () => {
-        setOpenDatePicker(!openDatePicker)
+
+function LogView() {
+    const [ip, setIp] = useState("")
+    const [startText, setStartText] = useState("날짜선택")
+    const [endText, setEndText] = useState("날짜선택")
+
+    const onChangeIp = (e) =>{
+        setIp(e.target.value);
+        console.log(ip);
+        dataAccess(ip, startText, endText);
     }
 
     return (
         <div>
-            IP <input type="text"/><br/><br/>
-            <button
-                onClick = {showDatePicker}/>
+            IP <input type="text" value={ip} onChange={onChangeIp
+            }/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            기간
             <div className="date">
-                <DatePickerOne showDatePicker={showDatePicker} />
+                <DatePickerOne 
+                    startText={startText} setStartText={setStartText}
+                    endText={endText} setEndText={setEndText}
+                />
             </div>
-            ---------------------------------------------------------
+            <br/><br/>
             <table border="1">
                 <th>IP</th>
                 <th>시간</th>
@@ -63,5 +127,4 @@ function LogView(props) {
     );
 }
     
-
 export default LogView;
